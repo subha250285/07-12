a1594540c4ddb370eb2c8ccef953b312d2c4b8aa
rakhi <jsubhashreem.gmail.com>
2024-12-07 23:46:26 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "ap-south-1"
 }
-resource "aws_s3_bucket" "rraa" {
-  bucket = "my-tf-test-bucket25"
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "amulyajspur"
 
   tags = {
     Name        = "My bucket25"
