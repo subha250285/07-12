47e119da3c3cf9f69b13db42854c166dc0c6e21d
rakhi <jsubhashreem.gmail.com>
2024-12-07 23:29:35 +0530

    updated

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 resource "aws_s3_bucket" "my_bucket" {
-  bucket  = "my-unique-bucket-name"
+  bucket  = "my-unique-bucket-name1"
   tags    = {
-	Name          = "MyS3Bucket1"
+	Name          = "MyS3Bucket1321"
 	Environment    = "Production"
   }
 }
